e089b26db67102795ce23e842a9837872920f667
Louis Klein <louisklein71@gmail.com>
2023-05-14 02:14:11 -0700

    test

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal");
+modal.classList = "hidden";
 
-
-
+const likeButtons = document.querySelectorAll('.like');
+likeButtons.forEach(
+  likeButton => likeButton.addEventListener('click', function(){
+    mimicServerCall().catch((error)=>{
+      modal.classList.remove("hidden");
+      modal.textContent = error;
+      setTimeout(function(){
+        modal.classList = "hidden";
+      }, 3000);
+    })
+    .then(data => {
+      const likeGlyph = likeButton.querySelector('.like-glyph');
+      if(likeGlyph.classList != "activated-heart"){
+        likeGlyph.textContent = FULL_HEART;
+        likeGlyph.classList = "activated-heart";
+      }
+      else{
+        likeGlyph.textContent = EMPTY_HEART;
+        likeGlyph.classList.remove("activated-heart");
+      }
+    }
+    )
+  })
+)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
